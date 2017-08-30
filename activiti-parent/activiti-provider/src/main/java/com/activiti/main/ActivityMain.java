9d917d64e8f0d96581c86edd3eb8d129d7ac987a
mayunliang <mayunliang@chtwm.com>
2017-08-30 11:00:10 +0800

    启动项目同时启动审批流程

--- activiti-parent/activiti-provider/src/main/java/com/activiti/main/ActivityMain.java
+++ activiti-parent/activiti-provider/src/main/java/com/activiti/main/ActivityMain.java
@@ -1,8 +1,12 @@
 package com.activiti.main;
 
+import com.activiti.service.PublishProcessService;
+import com.activiti.service.impl.PublishProcessServiceImp;
+import org.apache.log4j.Logger;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ActivityMain {
+	private static Logger logger=Logger.getLogger(ActivityMain.class);
 	public static void main(String[] args){
 //		com.alibaba.dubbo.container.Main.main(args);
 		//http://www.yiibai.com/javalang/runtime_addshutdownhook.html
@@ -11,8 +15,18 @@ public class ActivityMain {
 //		ApplicationContext ctx=new ClassPathXmlApplicationContext("applicationContext.xml");
 		 ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
 				 "classpath:spring/applicationContext.xml");
-
 	        context.start();
+
+	        PublishProcessService publishProcessService= (PublishProcessService) context.getBean("publishProcessServiceImp");
+		logger.info("启动北京大区审批流程开始");
+		publishProcessService.publish("beijingProcess.bpmn");
+		logger.info("北京大区审批流程启动结束");
+		logger.info("启动普通大区审批流程开始");
+		publishProcessService.publish("common.bpmn");
+		logger.info("普通大区审批流程启动结束");
+		logger.info("总部大区审批流程开始");
+		publishProcessService.publish("companyProcess.bpmn");
+		logger.info("总部大区审批流程启动结束");
 		System.out.print("-------------工作流dubbo服务启动成功---------------");
 	        synchronized (ActivityMain.class) {
 	            while (true) {
