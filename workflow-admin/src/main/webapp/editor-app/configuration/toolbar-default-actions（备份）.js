8a2456b1543624229230bb9302a4405550916529
houjinrong <houjinrong@chtwn.com>
2017-11-14 10:34:08 +0800

    修改模型编辑器自定义事件

--- workflow-admin/src/main/webapp/editor-app/configuration/toolbar-default-actions（备份）.js
+++ workflow-admin/src/main/webapp/editor-app/configuration/toolbar-default-actions（备份）.js
@@ -261,7 +261,7 @@ KISBPM.TOOLBAR = {
         },
         
         closeEditor: function(services) {
-            window.location.href = "/activiti/model/modelManager";
+        	window.location.href = "./";
         },
         
         /**
@@ -326,7 +326,7 @@ var SaveModelCtrl = [ '$rootScope', '$scope', '$http', '$route', '$location',
 
     $scope.saveAndClose = function () {
     	$scope.save(function() {
-    		window.location.href = "/activiti/model/modelManager";
+    		window.location.href = "./";
     	});
     };
     $scope.save = function (successCallback) {
