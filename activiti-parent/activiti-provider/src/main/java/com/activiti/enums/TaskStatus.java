78a3413d05253fa29b7b8b95a1e0464210173f01
houjinrong <houjinrong@chtwn.com>
2018-01-30 14:16:21 +0800

    我的任务状态

---
+++ activiti-parent/activiti-provider/src/main/java/com/activiti/enums/TaskStatus.java
@@ -0,0 +1,20 @@
+package com.activiti.enums;
+
+public enum TaskStatus {
+
+    //未完成
+    UNFINISHED("unfinished"),
+    //审核完成-通过
+    FINISHEDPASS("finished_pass"),
+    //审核完成-未通过
+    FINISHEDREFUSE("finished_refuse"),
+    //转办
+    TRANSFER("transferTask");
+
+    public String value;
+
+    TaskStatus(String value){
+        this.value = value;
+    }
+
+}
