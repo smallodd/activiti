342f0779288170dd834054994a4356bfde2bdc3b
houjinrong <houjinrong@chtwn.com>
2018-03-15 17:05:42 +0800

    静态任务-驳回适配

--- activiti-parent/activiti-provider/src/main/java/com/activiti/service/impl/WorkTaskV2ServiceImpl.java
+++ activiti-parent/activiti-provider/src/main/java/com/activiti/service/impl/WorkTaskV2ServiceImpl.java
@@ -907,24 +907,13 @@ public class WorkTaskV2ServiceImpl implements WorkTaskV2Service {
                     throw  new WorkFlowException(CodeConts.WORK_FLOW_NO_APPROVER,"操作失败，请在工作流管理平台将任务节点：'"+tUserTask.getTaskName()+"'设置审批人后再创建任务");
                 }
                 if(task.getTaskDefinitionKey().trim().equals(tUserTask.getTaskDefKey().trim())){
-                    if(StringUtils.isNotBlank(task.getAssignee())){
-                        Map<String,Object> variables = Maps.newHashMap();
-                        for(String candidateId : task.getAssignee().split(",")){
-                            variables.put(task.getTaskDefinitionKey()+":"+candidateId,candidateId+":"+TaskStatus.UNFINISHED.value);
-                        }
-                        taskService.setVariablesLocal(processInstanceId, variables);
-                    }
-
                     String candidateIds = tUserTask.getCandidateIds();
 
                     Map<String,Object> variable = Maps.newHashMap();
                     variable.put(tUserTask.getTaskDefKey()+":"+TaskVariable.TASKTYPE.value,tUserTask.getTaskType());
                     variable.put(tUserTask.getTaskDefKey()+":"+TaskVariable.TASKUSER.value,candidateIds);
 
-                    if (TaskType.CANDIDATEGROUP.value.equals(tUserTask.getTaskType())) {
-                        //组
-                        taskService.addCandidateGroup(task.getId(), tUserTask.getCandidateIds());
-                    } else if (TaskType.CANDIDATEUSER.value.equals(tUserTask.getTaskType())) {
+                    if (TaskType.CANDIDATEUSER.value.equals(tUserTask.getTaskType())) {
                         //候选人
                         for(String candidateId : candidateIds.split(",")){
                             variable.put(tUserTask.getTaskDefKey()+":"+candidateId,candidateId+":"+TaskStatus.UNFINISHED.value);
