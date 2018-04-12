f9f6df27d962d7918e71033020b143ec2ace47f1
mayunliang <mayunliang@chtwm.com>
2018-04-12 16:13:47 +0800

    新建controller

---
+++ workflow-admin/src/main/java/com/hengtian/flow/controller/WorkflowController.java
@@ -0,0 +1,15 @@
+package com.hengtian.flow.controller;
+
+import com.hengtian.common.result.Result;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by ma on 2018/4/12.
+ */
+@Controller
+@RequestMapping("rest/flow")
+public class WorkflowController {
+
+
+}
