bccddf8557c576cfb41169449899cdf6ad56b544
mayunliang <mayunliang@chtwm.com>
2018-04-17 10:01:11 +0800

    创建俩个controller类来编写方法

--- workflow-admin/src/main/java/com/hengtian/flow/controller/WorkflowOperateController.java
+++ workflow-admin/src/main/java/com/hengtian/flow/controller/WorkflowOperateController.java
@@ -6,10 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by ma on 2018/4/12.
+ * 所有涉及到操作类的功能都放到这里
  */
 @Controller
-@RequestMapping("rest/flow")
-public class WorkflowController {
+@RequestMapping("/rest/flow/operate")
+public class WorkflowOperateController {
 
 
 }
---
+++ workflow-admin/src/main/java/com/hengtian/flow/controller/WorkflowPageController.java
@@ -0,0 +1,11 @@
+package com.hengtian.flow.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by ma on 2018/4/17.
+ * 所有列表查询都放这里
+ */
+@RequestMapping("/rest/flow/page")
+public class WorkflowPageController {
+}
