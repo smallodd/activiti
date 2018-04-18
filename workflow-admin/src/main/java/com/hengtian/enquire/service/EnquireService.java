1eb36230942521bf406251ef042e753c93e684f3
chenzhangyan <1016724488@qq.com>
2018-04-18 14:44:52 +0800

    问询

---
+++ workflow-admin/src/main/java/com/hengtian/enquire/service/EnquireService.java
@@ -0,0 +1,7 @@
+package com.hengtian.enquire.service;
+
+/**
+ * @author chenzhangyan  on 2018/4/18.
+ */
+public interface EnquireService {
+}
--- workflow-admin/src/main/java/com/hengtian/flow/controller/WorkflowPageController.java
+++ workflow-admin/src/main/java/com/hengtian/flow/controller/WorkflowPageController.java
@@ -1,8 +1,7 @@
 package com.hengtian.flow.controller;
 
-import com.hengtian.common.operlog.SysLog;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by ma on 2018/4/17.
@@ -11,46 +10,4 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping("/rest/flow/page")
 public class WorkflowPageController {
-    /**
-     * 问询列表
-     *
-     * @return
-     */
-    @GetMapping("/enquireTaskList")
-    public String enquireTaskList() {
-        return "activiti/enquireTask";
-    }
-
-    /**
-     * 被问询列表
-     *
-     * @return
-     */
-    @GetMapping("/enquiredTaskList")
-    public String enquiredTaskList() {
-        return "activiti/enquiredTask";
-    }
-
-
-    /**
-     * 问询
-     *
-     * @return
-     */
-    @GetMapping("/enquire")
-    public String enquire() {
-        return "activiti/enquire";
-    }
-
-    /**
-     * 问询意见查询接口
-     *
-     * @return
-     */
-    @ResponseBody
-    @SysLog(value = "问询意见查询")
-    @PostMapping("/enquireComment/{taskId}")
-    public String enquireComment(@PathVariable String taskId) {
-        return "activiti/enquireComment";
-    }
 }
