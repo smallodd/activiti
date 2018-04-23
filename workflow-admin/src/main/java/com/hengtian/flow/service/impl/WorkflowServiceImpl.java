cf25bc679590335b180eeff800e93cbaf83db639
chenzhangyan <1016724488@qq.com>
2018-04-23 09:48:01 +0800

    @Service

--- workflow-admin/src/main/java/com/hengtian/flow/service/impl/WorkflowServiceImpl.java
+++ workflow-admin/src/main/java/com/hengtian/flow/service/impl/WorkflowServiceImpl.java
@@ -29,12 +29,14 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+@Service
 public class WorkflowServiceImpl implements WorkflowService {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
