1d202b2d70aac32357f8044b792f2f950a2dd169
mayunliang <mayunliang@chtwm.com>
2018-04-25 10:46:58 +0800

    模拟提交失败提示

--- workflow-admin/src/main/java/com/hengtian/flow/controller/WorkflowOperateController.java
+++ workflow-admin/src/main/java/com/hengtian/flow/controller/WorkflowOperateController.java
@@ -92,6 +92,7 @@ public class WorkflowOperateController extends WorkflowBaseController {
 
                 return workflowService.startProcessInstance(processParam);
             }catch (Exception e){
+                logger.error("模拟提交失败",e);
                 result.setMsg("模拟开启失败，请联系管理员进行排查！");
                 result.setCode(Constant.FAIL);
                 result.setSuccess(false);
