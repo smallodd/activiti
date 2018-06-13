89ab000e1f41c3a66251a0580cd7e2528ae33d2d
mayunliang <mayunliang@chtwm.com>
2018-06-13 17:39:39 +0800

    修改自定义设置审批人问题

--- workflow-admin/src/main/java/com/hengtian/flow/service/impl/WorkflowServiceImpl.java
+++ workflow-admin/src/main/java/com/hengtian/flow/service/impl/WorkflowServiceImpl.java
@@ -225,6 +225,9 @@ public class WorkflowServiceImpl extends ActivitiUtilServiceImpl implements Work
                 tWorkDetail.setOperTaskKey(historicTaskInstances.get(0).getName());
                 workDetailService.insert(tWorkDetail);
             } else {
+                for (int i = 0; i < taskList.size(); i++) {
+                    taskId+=taskList.get(0).getId();
+                }
                 log.info("业务平台设置审批人");
 
                 result.setSuccess(true);
