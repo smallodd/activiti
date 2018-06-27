0919477b882cbe838c340b691499870bcb89c23f
mayunliang <mayunliang@chtwm.com>
2018-06-27 20:47:05 +0800

    修改bug

--- workflow-admin/src/main/java/com/hengtian/flow/service/impl/WorkflowServiceImpl.java
+++ workflow-admin/src/main/java/com/hengtian/flow/service/impl/WorkflowServiceImpl.java
@@ -1027,7 +1027,10 @@ public class WorkflowServiceImpl extends ActivitiUtilServiceImpl implements Work
                 }
             }
         }
+       Task task= taskService.createTaskQuery().processInstanceId(hisTask.getProcessInstanceId()).taskDefinitionKey(targetTaskDefKey).singleResult();
         log.info("跳转成功");
+        Result result=new Result(true,Constant.SUCCESS,"跳转成功");
+        result.setObj(setButtons(TaskNodeResult.toTaskNodeResult(task)));
         return new Result(true,Constant.SUCCESS,"跳转成功");
     }
 
