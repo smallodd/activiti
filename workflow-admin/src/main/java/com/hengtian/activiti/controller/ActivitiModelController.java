827443201b7d0c3b141541e6acda781d2bc141b6
houjinrong <houjinrong@chtwn.com>
2018-07-05 19:30:59 +0800

    去掉多余参数

--- workflow-admin/src/main/java/com/hengtian/activiti/controller/ActivitiModelController.java
+++ workflow-admin/src/main/java/com/hengtian/activiti/controller/ActivitiModelController.java
@@ -535,7 +535,7 @@ public class ActivitiModelController extends BaseController {
         if(modelIds == null || modelIds.length == 0){
             return;
         }
-        JSONArray jsonArray = activitiModelService.exportModel(modelIds, "D:/");
+        JSONArray jsonArray = activitiModelService.exportModel(modelIds);
 
         byte[] bytes = jsonArray.toString().getBytes();
         try {
