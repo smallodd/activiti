176b27d0e162edefd0e3a5baa2df74d2aa9d9805
houjinrong <houjinrong@chtwm.com>
2019-06-13 11:10:45 +0800

    springboot改造

---
+++ workflow-admin/src/main/java/com/hengtian/config/MybatisConfiguration.java
@@ -0,0 +1,41 @@
+package com.hengtian.config;
+
+//import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+//import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
+import com.baomidou.mybatisplus.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.spring.MybatisSqlSessionFactoryBean;
+import org.apache.ibatis.plugin.Interceptor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * mybatis配置
+ * @ClassName MybatisConfiguration
+ * @Description
+ * @Author hour
+ * @Date2019/6/13 10:06
+ * @Version V1.0
+ */
+@Configuration
+public class MybatisConfiguration {
+
+    /**
+     * Spring整合Mybatis
+     */
+    @Bean
+    public MybatisSqlSessionFactoryBean mybatisSqlSessionFactoryBean(@Qualifier("dataSource") DataSource dataSource){
+        MybatisSqlSessionFactoryBean bean = new MybatisSqlSessionFactoryBean();
+        bean.setDataSource(dataSource);
+
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        paginationInterceptor.setDialectType("mysql");
+        Interceptor[] plugins = {paginationInterceptor};
+        bean.setPlugins(plugins);
+        return bean;
+    }
+
+
+}
