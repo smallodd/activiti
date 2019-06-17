92123abb37a97f3a44b4ed093ffdf7f68adc748e
houjinrong <houjinrong@chtwm.com>
2019-06-17 15:02:06 +0800

    springboot改造

--- workflow-admin/src/main/java/com/hengtian/config/DruidConfiguration.java
+++ workflow-admin/src/main/java/com/hengtian/config/DruidConfiguration.java
@@ -3,9 +3,7 @@ package com.hengtian.config;
 import com.alibaba.druid.pool.DruidDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -21,7 +19,6 @@ import javax.sql.DataSource;
  */
 @Slf4j
 @Configuration
-@EnableConfigurationProperties(DataSourceProperties.class)
 public class DruidConfiguration {
 
     /**
