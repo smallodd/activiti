062773a6f2e3f8676db1436e2bba10cad61f56fb
houjinrong <houjinrong@chtwm.com>
2019-06-18 11:10:03 +0800

    springboot改造

--- workflow-admin/src/main/java/com/hengtian/activiti/editor/ModelSaveRestResource.java
+++ workflow-admin/src/main/java/com/hengtian/activiti/editor/ModelSaveRestResource.java
@@ -14,14 +14,11 @@ import org.apache.batik.transcoder.TranscoderOutput;
 import org.apache.batik.transcoder.image.PNGTranscoder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
