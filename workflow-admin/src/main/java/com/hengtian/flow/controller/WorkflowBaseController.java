0b0a90673526f983390d0acb0e2aa7a9c7ac4648
houjinrong <houjinrong@chtwm.com>
2019-07-08 15:26:38 +0800

    审批人查询优化-后台

--- workflow-admin/src/main/java/com/hengtian/flow/controller/WorkflowBaseController.java
+++ workflow-admin/src/main/java/com/hengtian/flow/controller/WorkflowBaseController.java
@@ -235,8 +235,6 @@ public class WorkflowBaseController extends BaseRestController {
             jsonObject.put("id", t.getAssignee());
             jsonObject.put("text", t.getAssigneeName());
 
-
-
             if(AssignTypeEnum.ROLE.code.equals(t.getAssigneeType()) || AssignTypeEnum.EXPR.code.equals(t.getAssigneeType()) || StringUtils.isNotBlank(t.getAssigneeReal())){
                 jsonObject.put("state", "closed");
                 JSONArray jsonArray = new JSONArray();
