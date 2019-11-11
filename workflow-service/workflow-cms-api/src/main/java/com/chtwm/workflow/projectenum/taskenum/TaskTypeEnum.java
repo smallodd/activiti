5f12da4574679f6f137885e3df7601714273d3b1
fanyuexing <13581649370@126.com>
2019-11-11 16:20:45 +0800

    逍客任务实体类对应的枚举添加getset方法

--- workflow-service/workflow-cms-api/src/main/java/com/chtwm/workflow/projectenum/taskenum/TaskTypeEnum.java
+++ workflow-service/workflow-cms-api/src/main/java/com/chtwm/workflow/projectenum/taskenum/TaskTypeEnum.java
@@ -14,6 +14,22 @@ public enum TaskTypeEnum {
     public int code;
     public String value;
 
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
     TaskTypeEnum(int code, String value) {
         this.code = code;
         this.value = value;
