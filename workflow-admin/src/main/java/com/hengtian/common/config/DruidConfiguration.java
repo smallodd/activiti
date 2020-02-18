345428f7f46c4e3867952b25281f5f60ef3c8dd8
马云亮 <mayunliang@chtwm.com>
2020-02-18 10:57:17 +0800

    修改数据库链接配置

--- workflow-admin/src/main/java/com/hengtian/common/config/DruidConfiguration.java
+++ workflow-admin/src/main/java/com/hengtian/common/config/DruidConfiguration.java
@@ -3,7 +3,9 @@ package com.hengtian.common.config;
 import com.alibaba.druid.pool.DruidDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -18,7 +20,57 @@ import javax.sql.DataSource;
  */
 @Slf4j
 @Configuration
+@EnableConfigurationProperties
+
 public class DruidConfiguration {
+
+
+
+    @Value("${spring.datasource.driverClassName}")
+    private String driverClassName;
+    @Value("${spring.datasource.url}")
+    private String url;
+    @Value("${spring.datasource.username}")
+    private String userName;
+    @Value("${spring.datasource.password}")
+    private String password;
+    @Value("${spring.datasource.initialSize}")
+    private int initialSize;
+    @Value("${spring.datasource.maxActive}")
+    private int maxActive;
+    @Value("${spring.datasource.minIdle}")
+    private int minldle;
+    @Value("${spring.datasource.maxWait}")
+    private long maxWait;
+    @Value("${spring.datasource.validationQuery}")
+    private String validationQuery;
+    @Value("${spring.datasource.testOnBorrow}")
+    private boolean testOnBorrow;
+    @Value("${spring.datasource.testOnReturn}")
+    private boolean testOnReturn;
+    @Value("${spring.datasource.testWhileIdle}")
+    private boolean testWhiledle;
+    @Value("${spring.datasource.timeBetweenEvictionRunsMillis}")
+    private long timeBetweenEvictionRunsMillis;
+    @Value("${spring.datasource.minEvictableIdleTimeMillis}")
+    private long minEvictabledleTimeMillis;
+    @Value("${spring.datasource.removeAbandoned}")
+    private boolean removeAbandoned;
+    @Value("${spring.datasource.removeAbandonedTimeout}")
+    private int  removeAbandonedTimeout;
+    @Value("${spring.datasource.logAbandoned}")
+    private boolean logAbandoned;
+    @Value("${spring.datasource.name}")
+    private String datasourceName;
+    @Value("${spring.datasource.type}")
+    private String datasourceType;
+    @Value("${spring.datasource.maxOpenPreparedStatements}")
+    private int maxOpenPreparedStatements;
+
+
+
+
+
 
     /**
      * 功能描述:数据源
@@ -30,7 +82,29 @@ public class DruidConfiguration {
     @Primary
     @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource dataSource(){
-        return new DruidDataSource();
+        DruidDataSource druidDataSource = new DruidDataSource();
+        druidDataSource.setDriverClassName(driverClassName);
+        druidDataSource.setUrl(url);
+        druidDataSource.setUsername(userName);
+        druidDataSource.setPassword(password);
+        druidDataSource.setInitialSize(initialSize);
+        druidDataSource.setMaxActive(maxActive);
+        druidDataSource.setMinIdle(minldle);
+        druidDataSource.setMaxWait(maxWait);
+        druidDataSource.setValidationQuery(validationQuery);
+        druidDataSource.setTestOnBorrow(testOnBorrow);
+        druidDataSource.setTestOnReturn(testOnReturn);
+        druidDataSource.setTestWhileIdle(testWhiledle);
+        druidDataSource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
+        druidDataSource.setMinEvictableIdleTimeMillis(minEvictabledleTimeMillis);
+        druidDataSource.setRemoveAbandoned(removeAbandoned);
+        druidDataSource.setRemoveAbandonedTimeout(removeAbandonedTimeout);
+        druidDataSource.setLogAbandoned(logAbandoned);
+        druidDataSource.setName(datasourceName);
+        druidDataSource.setDbType(datasourceType);
+        druidDataSource.setMaxOpenPreparedStatements(maxOpenPreparedStatements);
+
+        return druidDataSource;
     }
 
     /**
