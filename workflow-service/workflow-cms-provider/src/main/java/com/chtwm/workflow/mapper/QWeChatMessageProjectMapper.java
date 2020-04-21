7a3789df77b6476a3e3d26dde076660e7fc71658
fanyuexing <13581649370@126.com>
2020-04-21 18:38:05 +0800

    添加企业微信审核所需要的相关接口方法/优化代码

--- workflow-service/workflow-cms-provider/src/main/java/com/chtwm/workflow/mapper/QWeChatMessageProjectMapper.java
+++ workflow-service/workflow-cms-provider/src/main/java/com/chtwm/workflow/mapper/QWeChatMessageProjectMapper.java
@@ -1,41 +1,37 @@
 package com.chtwm.workflow.mapper;
 
-
 import com.chtwm.workflow.entity.TaskModelVO;
-import com.chtwm.workflow.entity.TaskNoticePO;
-import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 /**
  * @author fanyuexing
- * @date 2019/11/8 9:25
+ * @date 2020/4/21 17:39
  */
-@Mapper
-public interface XiaoKeMessageProjectMapper {
+public interface QWeChatMessageProjectMapper {
 
     /**
-     * 获取所有纷享销客待发送和发送失败的消息
+     * 获取所有企业微信待发送和发送失败的消息
      * @return
      */
     List<TaskModelVO> getAllUnSendMessage();
 
     /**
-     * 更新纷享逍客的消息发送的结果
+     * 更新企业微信的消息发送的结果
      * @param id 主键
      * @param noticeState  待修改的状态
      * @return
      */
-    Integer updateThisMessageNoticeState(@Param("id") Long id,@Param("noticeState") Integer noticeState);
+    Integer updateThisMessageNoticeState(@Param("id") Long id, @Param("noticeState") Integer noticeState);
 
     /**
-     * 根据主键集合更新纷享逍客的消息发送的结果
+     * 根据主键集合更新企业微信的消息发送的结果
      * @param ids 主键集合
      * @param noticeState 待修改的状态
      * @return
      */
     Integer updateNoticeStateByListKeys(@Param("ids") List<Long> ids,@Param("noticeState") Integer noticeState);
+
 
 }
