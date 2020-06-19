0cfea4dd78d19baef621fa1d564f3f03e7870b28
马云亮 <mayunliang@chtwm.com>
2020-06-19 09:59:37 +0800

    修改问题

--- workflow-admin/src/main/java/com/hengtian/flow/extend/TaskManager.java
+++ workflow-admin/src/main/java/com/hengtian/flow/extend/TaskManager.java
@@ -2,8 +2,9 @@ package com.hengtian.flow.extend;
 
 import com.hengtian.common.param.TaskActionParam;
 import com.hengtian.common.result.Result;
+import com.hengtian.flow.service.WorkflowService;
 
 public interface TaskManager {
 
-    Result taskAction(TaskActionParam taskActionParam);
+    Result taskAction(TaskActionParam taskActionParam, WorkflowService workflowService);
 }
